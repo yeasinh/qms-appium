bba31fa9cd5b922641c1fe4f05591576573702c1
yeasinh <yeasinhossain.246@gmail.com>
2024-01-31 16:53:53 +0600

    started project

---
+++ src/main/java/com/yeasin/appium_qms/App.java
@@ -0,0 +1,12 @@
+package com.yeasin.appium_qms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
